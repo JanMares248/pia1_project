9c63a8ce3e080d9230f09aa9a1ba80082b1c3cce
JanMares248 <jenda.mares@centrum.cz>
2025-11-24 09:59:01 +0100

    pridan form pro upravu hmotnosti

---
+++ Form4.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "Form3.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+using namespace System::Drawing;
+using namespace System::Collections::Generic;
+
+public ref class Form4 : public Form
+{
+public:
+    Form4(System::Drawing::Size s);
+
+    static List<int>^ savedSizes = gcnew List<int>();
+
+private:
+    int baseRadius;
+    int selectedIndex;
+    List<TextBox^>^ sizeEditors;
+
+
+    int getRadius(int sizeSetting);
+
+    void loadSizes();
+
+
+    void OnPaint(Object^ sender, PaintEventArgs^ e);
+    void OnMouseDown(Object^ sender, MouseEventArgs^ e);
+    void SizeEditor_TextChanged(Object^ sender, EventArgs^ e);
+
+    Button^ btnPrev;
+    void btnPrev_Click(Object^ sender, EventArgs^ e);
+};
