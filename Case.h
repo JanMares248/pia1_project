1d7a9c0103073a5351e8cf574b329a005844af5d
rycj <kubikryc@gmail.com>
2025-12-03 16:32:58 +0100

    Solver works! (maybe...)

--- Case.h
+++ Case.h
@@ -75,62 +75,15 @@ class Case {
 	
 	double Distance(int i, int j)
 	{
-		double v = sqrt(pow(Telesa[i].r[0]-Telesa[j].r[0],2.0) + pow(Telesa[i].r[1]-Telesa[j].r[1],2.0));
+		double s = 0;
+		for (int k = 0; k < dim; k++)
+		{
+			// std::cout<<s<<std::endl;
+			s+=pow(abs(Telesa[j].r[k]-Telesa[i].r[k]),2);
+		}
+		// std::cout<<"end"<<std::endl;
+		double v = sqrt(s);
 		return v;
-	}
-	void Dr()
-	{
-		// dr;
-		// for (int d = 0; d < dim;d++)
-		// {
-		// 	dr[d] = 0.0;
-		// }
-		for (int d = 0; d < dim;d++) // pro každou souřadnici
-		{
-			for (int i = 0;i < Telesa.size();i++) // pro každé těleso
-			{
-
-				Telesa[i].dr[d] = Telesa[i].p[d] / Telesa[i].m;
-				// if (d==1 && i==1)
-				// {
-				// std::cout <<Telesa[i].p[d]<< std::endl;
-				// std::cout <<Telesa[i].m<< std::endl;
-				// std::cout <<Telesa[i].dr[d]<< std::endl;
-				// std::cout <<"\n"<< std::endl;
-				// }
-				
-
-
-			}
-		}
-	}
-	void Dp()
-	{
-		// for (int d = 0; d < dim;d++)
-		// {
-		// 	dp[d] = 0.0;
-		// }
-		for (int d = 0; d < dim;d++)
-		{
-			for (int i = 0;i < dim;i++)
-			{
-				Telesa[i].dp[d] = 0;
-				for (int j = 0;j < dim;j++)
-				{
-					if (i != j)
-					{
-						Telesa[i].dp[d] = Telesa[i].dp[d] + (-6.6743e-11 * Telesa[i].m * Telesa[j].m / pow(Distance(i, j), 3)) * (Telesa[i].x - Telesa[j].x);
-					}
-					if (d==0)
-					{
-					std::cout <<Telesa[i].p[d]<< std::endl;
-					std::cout <<Telesa[i].m<< std::endl;
-					std::cout <<Telesa[i].dr[d]<< std::endl;
-					std::cout <<"\n"<< std::endl;
-					}
-				}
-			}
-		}
 	}
 	double Dv(int telI,int coor){
 		double dvi=0;
@@ -140,116 +93,18 @@ class Case {
 		{
 			if (j!=telI)
 			{
-				dvi+=(G*Telesa[telI].m*Telesa[j].m*(Telesa[j].r[coor]-Telesa[telI].r[coor]))/(pow(Telesa[j].r[coor]-Telesa[telI].r[coor],3)*Telesa[telI].m);
+				dvi-=(G*Telesa[j].m*(Telesa[j].r[coor]-Telesa[telI].r[coor]))/(pow(Distance(telI,j),3));
 			}
 		}
 		return dvi;
 		
 	}
 	void SolveEuSymp(double timeStep, double end, int saveInterval)
-	{
-		std::ofstream file("solverResult.txt");
-		// print
-											for (int i = 0; i < Telesa.size(); i++)
-											{
-												for (int j = 0; j < dim; j++)
-												{
-													file << Telesa[i].x[j];
-													if (j==dim-1)
-														{
-															file << "\n";
-														}
-														else
-														{
-															file << ";";
-														}
-													}
-
-											}
-											file << "\n\n";
-		int nSteps = ceil(end / timeStep); //vim bude zaokrohlovat
-		std::cout <<"Create time"<< std::endl;
-		for (int t = 0; t < nSteps; t++)  //mirne nechapu prevod z shift space do souradnic
-			{
-				// Telesa[i].Dp();
-				// Telesa[i].Dr();  // update hodnot v každý čas
-				for (int i = 0; i < Telesa.size(); i++) // pro každé těleso
-					{
-						Dp();
-						Dr();  // update hodnot v každý čas
-						Telesa[i].PolTransReversible(true);
-						for (int j = 0; j < dim; j++) // pro každou souřadnici
-						{
-							Telesa[i].np[j] = Telesa[i].p[j] + timeStep * Telesa[i].dp[j]; // spočte nové p a nové r
-							Telesa[i].nr[j] = Telesa[i].r[j] + timeStep * Telesa[i].dr[j];
-							if (t%saveInterval==0)
-							{
-								// print
-										{
-			
-											for (int i = 0; i < Telesa.size(); i++)
-											{
-												for (int j = 0; j < dim; j++)
-												{
-													file << Telesa[i].x[j];
-													if (j==dim-1)
-														{
-															file << "\n";
-														}
-														else
-														{
-															file << ";";
-														}
-													}
-
-											}
-											file << "\n\n";
-												// pro pripad, ze bychom chteli vypisovat i p
-											// 	for (int j = 0; j < dim; j++)
-											// 	{
-											// 		MyFile << Telesa[i].np[j];
-											// 		if (j==dim-1)
-											// 			{
-											// 				file << "\n\n";
-											// 			}
-											// 			else
-											// 			{
-											// 				file << ";";
-											// 			}
-											// 		}
-											// file << Telesa[i].dp;
-											// }
-											// file << "\n\n";
-										} // zapíše do souboru
-															} 
-														}
-													}
-			}
-		for (int i = 0; i < Telesa.size(); i++)//for (Teleso t : Telesa) //-> nahrat nove do stare vrstvy, asi chybi.
-		{
-			for (int j = 0; j < dim; j++) // pro každou souřadnici
-						{
-			Telesa[i].p[j] = Telesa[i].np[j];
-			Telesa[i].r[j] = Telesa[i].nr[j];
-			}
-			//t.p = t.np;
-		}
-
-	//pokud budete chtit dodelat solvery, tak mohme tady -> mluvil o normalnim eulerovy
-};
-	void SolveEuSymp2(double timeStep, double end, int saveInterval)
 	{	
 		std::ofstream file("solverResult.txt");
 		int Nsteps=end/timeStep;
 		for (int t = 0; t < Nsteps; t++)	//casova iterace
 		{
-			// std::cout <<"puvodni"<< std::endl;
-			// std::cout <<Telesa[1].p[0]<< std::endl;
-			// std::cout <<Telesa[1].p[1]<< std::endl;
-			// std::cout <<"\n dv"<< std::endl;
-			// std::cout <<Dv(1,0)<< std::endl;
-			// std::cout <<Dv(1,1)<< std::endl;
-			// std::cout <<"\n novy"<< std::endl;
 			for (int telI = 0; telI < Telesa.size(); telI++)	//telesova iterace
 			{
 				for (int coor = 0; coor < dim; coor++)	//souradincova iterace rychlost
@@ -279,9 +134,6 @@ class Case {
 					}
 				}
 			}
-			// std::cout <<Telesa[1].p[0]<< std::endl;
-			// std::cout <<Telesa[1].p[1]<< std::endl;
-			// std::cout <<"\n\n"<< std::endl;
 			if (t%saveInterval==0){
 				file << "\n";
 			}
--- main_BezGr.cpp
+++ main_BezGr.cpp
@@ -2,11 +2,11 @@
 #include "Case.h"
 int main(){
     Case c1;
-    Teleso t1(-5,100,0,3,6e10);
-    Teleso t2(5,-100,0,-3,4e10);
+    Teleso t1(147.5e9,0,0,29.8e3,6e24);
+    Teleso t2(0,0,0,0,1.989e30);
     c1.Add(t1);
     c1.Add(t2);
-    c1.SolveEuSymp2(1,100,10);
+    c1.SolveEuSymp(1,50000,500);
     // for (size_t i = 0; i < 2; i++)
     // {
     //     std::cout <<t1.r[i]<< std::endl;
